c4ad1c97a4dd1f3f3a51d9a12400aac866c21221
Vitor Britto <code@vitorbritto.com.br>
2018-09-15 11:04:53 -0300

    First Release

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './scss/main.scss'
+console.log('Welcome to Webpack')
+
+// Start coding... =]
---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const WebpackMd5Hash = require('webpack-md5-hash')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
+module.exports = {
+  entry: {
+    main: './src/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[chunkhash].js'
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx', '.json'],
+    alias: {
+      '@helpers': path.resolve(__dirname, 'src/helpers/'),
+      '@views': path.resolve(__dirname, 'src/views/'),
+      '@styles': path.resolve(__dirname, 'src/scss/')
+    }
+  },
+  module: {
+    rules: [
+      // JavaScript / ES6
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      // Styles
+      {
+        test: /\.scss$/,
+        use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
+      },
+      // Fonts
+      {
+        test: /\.(woff|woff2|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192
+            }
+          }
+        ]
+      },
+      // Images
+      // Inline base64 URLs for <=8k images, direct URLs for the rest
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin('dist', {}),
+    new MiniCssExtractPlugin({
+      filename: 'style.[contenthash].css',
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      hash: true,
+      template: './src/index.html',
+      filename: 'index.html'
+    }),
+    new WebpackMd5Hash()
+  ]
+}
